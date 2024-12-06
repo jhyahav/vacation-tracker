1358e55be0a501fdae066a2b0f2e678629b0b577
Jonathan <jhyahav2@gmail.com>
2024-12-06 19:13:23 +0200

    create SkylinePhoto component with skeleton placeholder

---
+++ src/components/SkylinePhoto.tsx
@@ -0,0 +1,32 @@
+import type { FC } from "react"
+import { Card, CardMedia, Skeleton } from "@mui/material"
+
+import { useUnsplashPhotos } from "../hooks/queries/useUnsplashPhoto"
+
+type Props = { cityName: string }
+
+export const SkylinePhoto: FC<Props> = ({ cityName }) => {
+  const query = `${cityName} skyline`
+
+  const { data: photo, isLoading } = useUnsplashPhotos(query)
+
+  return (
+    <Card sx={{ width: 345, margin: "16px" }}>
+      {isLoading ? (
+        <Skeleton
+          animation="wave"
+          height={194}
+          variant="rectangular"
+          width="100%"
+        />
+      ) : (
+        <CardMedia
+          alt={`Photo by ${photo?.user.name}`}
+          component="img"
+          height="194"
+          image={photo?.urls.regular}
+        />
+      )}
+    </Card>
+  )
+}
