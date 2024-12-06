feea989ada3aa3aa4608836b5758de040e722af4
Jonathan <jhyahav2@gmail.com>
2024-12-06 19:11:11 +0200

    wrap app content in query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "./App.css"
+import { SkylinePhoto } from "./components/SkylinePhoto"
+
+const queryClient = new QueryClient()
 
 const App = () => {
-  return <div>Placeholder</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>Placeholder</div>
+      <SkylinePhoto cityName="Tokyo" />
+    </QueryClientProvider>
+  )
 }
 
 export default App
