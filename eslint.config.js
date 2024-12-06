ccff08dbcd4852bbb76a777538ee52f7a24e449f
Jonathan <jhyahav2@gmail.com>
2024-12-06 17:24:35 +0200

    initial commit: bootstrap with vite, install deps, configure prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-undef": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      eqeqeq: ["error", "always"],
+      "no-shadow": "error",
+      "consistent-return": "error",
+      "prefer-const": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-sort-props": [
+        "error",
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      "react/self-closing-comp": "error",
+      "react/jsx-no-useless-fragment": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+]
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css"
+
+const App = () => {
+  return <div>Placeholder</div>
+}
+
+export default App
