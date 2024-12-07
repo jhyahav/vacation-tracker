aabfab66edab4060ddee09b90a6c6bf4a62dae12
Jonathan <jhyahav2@gmail.com>
2024-12-07 15:44:36 +0200

    add logout button

---
+++ src/components/auth/LogOutButton.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react"
+import { Button } from "@mui/material"
+
+import { useAuth } from "../../global-state/AuthContext"
+
+export const LogOutButton: FC = () => {
+  const { user, signOut } = useAuth()
+
+  // The button shouldn't render if the user isn't logged in
+  if (!user) return null
+
+  const handleClick = () => signOut()
+
+  return (
+    <Button color="primary" variant="contained" onClick={handleClick}>
+      Log Out
+    </Button>
+  )
+}
