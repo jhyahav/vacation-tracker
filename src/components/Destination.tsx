7debd6a5c684dcc7dee8842bb88c13c959d770d2
Jonathan <jhyahav2@gmail.com>
2024-12-07 12:40:31 +0200

    create rudimentary destination component

---
+++ src/components/Destination.tsx
@@ -0,0 +1,39 @@
+import type { FC } from "react"
+import { SkylinePhoto } from "./SkylinePhoto"
+import { useWeather } from "../hooks/queries/useWeather"
+import { WeatherWidget } from "./WeatherWidget"
+
+type Props = { cityName: string }
+
+export const Destination: FC<Props> = ({ cityName }) => {
+  const { data, isLoading } = useWeather(cityName)
+
+  const {
+    main: { temp } = {},
+    weather: [{ main: conditions, description, icon }] = [{}],
+    sys: { country: countryCode } = {},
+    name,
+  } = data ?? {}
+
+  const regionNames = new Intl.DisplayNames(["en"], { type: "region" })
+  const countryName = countryCode ? regionNames.of(countryCode) : undefined
+
+  return (
+    <div>
+      {!isLoading ? (
+        <h1>
+          {name}, {countryName}
+        </h1>
+      ) : null}
+
+      <SkylinePhoto cityName={cityName} />
+      <WeatherWidget
+        conditions={conditions}
+        description={description}
+        icon={icon}
+        isLoading={isLoading}
+        temp={temp}
+      />
+    </div>
+  )
+}
