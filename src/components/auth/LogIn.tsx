9f36ea7f92853dadc454fb56dac487bfc21a44d5
Jonathan <jhyahav2@gmail.com>
2024-12-08 22:46:40 +0200

    add flex wrapper for LogIn component

--- src/components/auth/LogIn.tsx
+++ src/components/auth/LogIn.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 
 import { Button, TextField, Typography, Alert } from "@mui/material"
 
-import { StyledFormBox } from "../styledComponents"
+import { StyledFormBox, StyledLogInContainer } from "../styledComponents"
 import { getAuthErrorMessage } from "../../utils/getAuthErrorMessage"
 import { PasswordRequirementList } from "./PasswordRequirementList"
 
@@ -58,58 +58,60 @@ export const LogIn: FC<Props> = ({
   }
 
   return (
-    <StyledFormBox component="form" onSubmit={handleLogin}>
-      <Typography gutterBottom align="center" variant="h4">
-        {title}
-      </Typography>
+    <StyledLogInContainer>
+      <StyledFormBox component="form" onSubmit={handleLogin}>
+        <Typography gutterBottom align="center" variant="h4">
+          {title}
+        </Typography>
 
-      {error ? <Alert severity="error">{error}</Alert> : null}
+        {error ? <Alert severity="error">{error}</Alert> : null}
 
-      <TextField
-        required
-        label="Email"
-        slotProps={{ htmlInput: { "data-testid": "email" } }}
-        type="email"
-        value={email}
-        variant="outlined"
-        onChange={handleInputChange(setEmail)}
-      />
-      <TextField
-        required
-        label="Password"
-        slotProps={{ htmlInput: { "data-testid": "password" } }}
-        type="password"
-        value={password}
-        variant="outlined"
-        onChange={handleInputChange(setPassword)}
-      />
-      {shouldConfirmPassword ? (
-        <>
-          <TextField
-            required
-            label="Confirm Password"
-            slotProps={{ htmlInput: { "data-testid": "confirm-password" } }}
-            type="password"
-            value={confirmPassword}
-            variant="outlined"
-            onChange={handleInputChange(setConfirmPassword)}
-          />
-          <PasswordRequirementList
-            confirmPassword={confirmPassword}
-            password={password}
-            updatePasswordValidity={updatePasswordValidity}
-          />
-        </>
-      ) : null}
-      <Button
-        color="primary"
-        disabled={isSubmitDisabled}
-        type="submit"
-        variant="contained"
-      >
-        {isLoading ? loadingText : title}
-      </Button>
-      {children}
-    </StyledFormBox>
+        <TextField
+          required
+          label="Email"
+          slotProps={{ htmlInput: { "data-testid": "email" } }}
+          type="email"
+          value={email}
+          variant="outlined"
+          onChange={handleInputChange(setEmail)}
+        />
+        <TextField
+          required
+          label="Password"
+          slotProps={{ htmlInput: { "data-testid": "password" } }}
+          type="password"
+          value={password}
+          variant="outlined"
+          onChange={handleInputChange(setPassword)}
+        />
+        {shouldConfirmPassword ? (
+          <>
+            <TextField
+              required
+              label="Confirm Password"
+              slotProps={{ htmlInput: { "data-testid": "confirm-password" } }}
+              type="password"
+              value={confirmPassword}
+              variant="outlined"
+              onChange={handleInputChange(setConfirmPassword)}
+            />
+            <PasswordRequirementList
+              confirmPassword={confirmPassword}
+              password={password}
+              updatePasswordValidity={updatePasswordValidity}
+            />
+          </>
+        ) : null}
+        <Button
+          color="primary"
+          disabled={isSubmitDisabled}
+          type="submit"
+          variant="contained"
+        >
+          {isLoading ? loadingText : title}
+        </Button>
+        {children}
+      </StyledFormBox>
+    </StyledLogInContainer>
   )
 }
