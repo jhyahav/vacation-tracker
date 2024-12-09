ab330dea32b494217c2a817f845e1cf184bf623b
Jonathan <jhyahav2@gmail.com>
2024-12-09 19:40:20 +0200

    add util dependency, add text encoder and decoder to jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom"
+import { TextEncoder, TextDecoder } from "util"
+
+globalThis.TextEncoder = TextEncoder
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+globalThis.TextDecoder = TextDecoder as any
